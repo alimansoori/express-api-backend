7fec036dd9026032b82ea9bc916ca74d63996bbb
ali <alimanssouri221@gmail.com>
2024-05-27 17:07:57 +0330

    feat: Add middlewares to include morgan logging

--- src/app/middlewares/index.ts
+++ src/app/middlewares/index.ts
@@ -1,10 +1,17 @@
 import bodyParser from "body-parser";
+import morgan from "../../config/morgan";
+import config from "../../config/config";
 
 export const applyMiddlewares = (app: any) => {
     const middlewares = [
         bodyParser.urlencoded({extended: true}),
         bodyParser.json()
     ];
+
+    if (config.env !== 'test') {
+        app.use(morgan.successHandler);
+        app.use(morgan.errorHandler);
+    }
 
     for (let middleware of middlewares) {
         app.use(middleware);
